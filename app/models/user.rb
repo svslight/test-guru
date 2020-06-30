cbd64610e85e410f0ae70f0824ba54dd4c7314b1
Svetlana Solodovnikova <svslight@yandex.ru>
2020-06-30 21:19:32 +0500

    Task 4. Query interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_tests
+  
+  def get_level_tests(level)
+    Test.select(:title).where(level: level).joins('JOIN user_tests ON test_id = tests.id')
+  end  
 end
