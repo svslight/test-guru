7ee0c1629d0e7820fbe988144035ecbb1014c363
svslight <35137363+svslight@users.noreply.github.com>
2020-06-30 23:07:51 +0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :user_tests
   
   def get_level_tests(level)
-    Test.select(:title).where(level: level).joins('JOIN user_tests ON test_id = tests.id')
+    #Test.select(:title).where(level: level).joins('JOIN user_tests ON test_id = tests.id').where('user_tests.user_id = ?', id)
+    Test.select(:title).where(level: level).joins(:user_tests).where('user_tests.user_id = ?', id)
   end  
 end
