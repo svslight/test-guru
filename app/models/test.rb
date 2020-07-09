9b26c08ab17d56e8db808a0fc3afad3bdbf70706
svslight <35137363+svslight@users.noreply.github.com>
2020-07-09 10:48:39 +0500

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,21 +8,12 @@ class Test < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  validate :validate_max_level, on: :create
-
   scope :easy, -> { where(level: 0..1).order(level: :asc) }
   scope :mid, -> { where(level: 2..4).order(level: :asc) }
-  scope :hard, -> { where(level: 5..Float::INFINITY).order(level: :asc) }
-  
+  scope :hard, -> { where(level: 5..Float::INFINITY).order(level: :asc) }  
   scope :get_category_tests, ->(name) { joins(:category).where(categories: {title: name}) }
 
   def self.order_by(name)
     get_category_tests(name).order(title: :desc).pluck(:title)
-  end
-
-  private
-
-  def validate_max_level
-    errors.add(:level) if level.to_i > 10
   end
 end
