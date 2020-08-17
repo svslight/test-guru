52bff4fe246c453cba58f676575fad69cd1bdd46
Svetlana Solodovnikova <svslight@yandex.ru>
2020-08-17 18:39:53 +0500

    Task 15. JavaScript Fixed

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -9,42 +9,41 @@ function sortRowsByTitle() {
   var table = document.querySelector('table')
 
   // NodeList
-	// https://developer.mozilla.org/ru/docs/Web/API/NodeList
+  // https://developer.mozilla.org/ru/docs/Web/API/NodeList
   var rows = table.querySelectorAll('tr')
-  // console.log(rows)
   var sortedRows = []
   
-  // select all table rows except the first one which is the header
+  // Select all table rows except the first one which is the header
   for (var i = 1; i < rows.length; i++) {
     sortedRows.push(rows[i])
   }
 
-  // check if the arrow is hidden then the grade is in ascending order
-  // control visibility of the arrows
+  // Check if the arrow is hidden then the grade is in ascending order
+  // Control visibility of the arrows
   if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
     sortedRows.sort(compareRowsAsc)
     this.querySelector('.octicon-arrow-up').classList.remove('hide')
     this.querySelector('.octicon-arrow-down').classList.add('hide')
   } else {
-      sortedRows.sort(compareRowsDesc)
-      this.querySelector('.octicon-arrow-down').classList.remove('hide')
-      this.querySelector('.octicon-arrow-up').classList.add('hide')
+    sortedRows.sort(compareRowsDesc)
+    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+    this.querySelector('.octicon-arrow-up').classList.add('hide')
   }
 
-  // creating a new html document in memory
+  // Creating a new html document in memory
   var sortedTable = document.createElement('table')
 
-  // adding a css class using JS
+  // Adding a css class using JS
   sortedTable.classList.add('table')
-  // adding the first row of the title from the rows collection
+  // Adding the first row of the title from the rows collection
   sortedTable.appendChild(rows[0])
 
-  // add the sorted array to the table in memory
+  // Add the sorted array to the table in memory
   for (var i = 0; i < sortedRows.length; i++) {
     sortedTable.appendChild(sortedRows[i])
   }
 
-  // put a table from memory into an existing
+  // Put a table from memory into an existing
   table.parentNode.replaceChild(sortedTable, table)
 }
 
