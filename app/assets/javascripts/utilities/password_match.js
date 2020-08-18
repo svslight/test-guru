05b1ca010339c84134739082b40daae90d940f91
Svetlana Solodovnikova <svslight@yandex.ru>
2020-08-18 09:55:39 +0500

    Task 15. JavaScript Fixed_

--- app/assets/javascripts/utilities/password_match.js
+++ app/assets/javascripts/utilities/password_match.js
@@ -4,17 +4,18 @@ document.addEventListener('turbolinks:load', function() {
 });
 
 function matchPasswordConfirmation() {
+
   if (document.getElementById('password-confirmation-field').value == '') {
     document.querySelector('.octicon-thumbsup').classList.add('hide');
     document.querySelector('.octicon-circle-slash').classList.add('hide');
-  } else if (document.getElementById('password-field').value != document.getElementById('password-confirmation-field').value) {
+    return ''
+  }
+
+  if (document.getElementById('password-field').value != document.getElementById('password-confirmation-field').value) {
     document.querySelector('.octicon-circle-slash').classList.remove('hide');
-    document.querySelector('.octicon-thumbsup').classList.add('hide');  
-  } else if (document.getElementById('password-field').value == document.getElementById('password-confirmation-field').value) {
+    document.querySelector('.octicon-thumbsup').classList.add('hide');
+  } else {
     document.querySelector('.octicon-thumbsup').classList.remove('hide');
     document.querySelector('.octicon-circle-slash').classList.add('hide');
-  } else {
-    document.querySelector('.octicon-circle-slash').classList.add('hide');
-    document.querySelector('.octicon-thumbsup').classList.add('hide');
-  }
+  }  
 }
