af6fac8782c2978c747fe8a0d775008f0ea62b02
Svetlana Solodovnikova <svslight@yandex.ru>
2020-08-24 16:03:04 +0500

    Task 17. Deploy to Heroku. ActionMailer for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'agile-castle-20258.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     adress:               'smtp.gmail.com',
     port:                 587,
