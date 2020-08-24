2e041edf41445ec2cd8be237f54377532f1f14d6
Svetlana Solodovnikova <svslight@yandex.ru>
2020-08-24 19:30:52 +0500

    Task 17. Deploy to Heroku. ActionMailer for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    # domain:               "agile-castle-20258.herokuapp.com",
+    domain:               "agile-castle-20258.herokuapp.com",
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
