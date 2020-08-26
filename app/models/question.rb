dca276674be8d8ae5149f254c3d7870a1d0b911d
Svetlana Solodovnikova <svslight@yandex.ru>
2020-08-26 19:55:19 +0500

    Task 17. Deploy to Heroku. Added Feedback form. Fixed

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :gists, dependent: :destroy
 
   validates :body, presence: true
